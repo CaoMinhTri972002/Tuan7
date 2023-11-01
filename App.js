c262b6afa6723e35e83ef4e51f881f44e772cbe0
CaoMinhTri972002 <144448561+CaoMinhTri972002@users.noreply.github.com>
2023-11-01 16:50:17 +0700

    Update

--- App.js
+++ App.js
@@ -1,9 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import PageChat from './PageChat'
+import { NavigationContainer } from '@react-navigation/native';
+import { Octicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import PageList from './PageList';
+import PageChat from './PageChat';
+const BottomTab = createBottomTabNavigator();
+
 export default function App() {
+
   return (
-<PageChat/>
+    <NavigationContainer>
+      <BottomTab.Navigator screenOptions={{headerShown: false, tabBarStyle: {backgroundColor: "#1BA9FF"}}}
+        
+      >
+
+        <BottomTab.Screen name='PageChat' component={PageChat}
+          
+          options={{
+            title: "",
+            tabBarIcon: ({focused, color})=>{
+            return <Octicons name="three-bars" size={24} color="black" />
+          }}}
+        />
+
+        <BottomTab.Screen name='PageList' component={PageList}
+          options={{
+            title: "",
+            tabBarIcon: ({focused, color})=>{
+              return <Octicons name="home" size={24} color="black" />
+            }}}
+        />
+        
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 }
 
--- PageChat.js
+++ PageChat.js
@@ -1,342 +1,167 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Pressable } from 'react-native';
-import { Image } from 'react-native';
-import { Button} from 'react-native';
-import React from 'react';
-import { TextInput } from 'react-native';
-
-
-export default function App() {
-    return (
-      <View style={styles.container}>
-    
-
-        <StatusBar style="auto" />
-          <View style={styles.style1}>
-            <View style={styles.chatview}>
-    <Pressable> <Image source={require('./assets/arrow.png')}
-                  style={{width:24,height:24}}>
-         
-      </Image></Pressable>
-<Text style={{color:'white',position:'absolute', left:180, top:10}}>Chat</Text>
-<Image source={require('./assets/gioHang.png')}
-      style={{width:24,height:24, position:'absolute', left: 350}}
-    ></Image>
- </View>
+import { useState } from 'react'
+import { View, Text, StatusBar, SafeAreaView, ScrollView, Pressable, Image, FlatList } from 'react-native'
 
 
 
-</View>
+export default function ChatWithSuplier({navigation}) {
+    const [selectedID, setSelectedId] = useState(0);
+    const [datas, setDatas] = useState([
+        {
+            name: "Ca nấu lẩu, nấu mì mini...",
+            shopName: "Devang",
+            isHotShop: true,
+            img: require("./assets/ca_nau_lau.png")
+        },
+        {
+            name: "1KG KHÔ GÀ BƠ TỎI",
+            shopName: "LTD FOOD",
+            isHotShop: true,
+            img: require("./assets/ga_bo_toi.png")
+        },
+        {
+            name: "Xe cần cẩu đa năng",
+            shopName: "Thế giới đồ chơi",
+            isHotShop: false,
+            img: require("./assets/xa_can_cau.png")
+        },
+        {
+            name: "Đồ chơi dạng mô hình",
+            shopName: "Thế giới đồ chơi",
+            isHotShop: false,
+            img: require("./assets/do_choi_dang_mo_hinh.png")
+        },
+        {
+            name: "Lãnh đạo giản đơn",
+            shopName: "Minh Long Book",
+            isHotShop: false,
+            img: require("./assets/lanh_dao_gian_don.png")
+        },
+        {
+            name: "Hiểu lòng trẻ con",
+            shopName: "Minh Long Book",
+            isHotShop: false,
+            img: require("./assets/hieu_long_con_tre.png")
+        },
+        {
+            name: "Donal Trump thiên tài lãnh đạo",
+            shopName: "Minh Long Book",
+            isHotShop: false,
+            img: require("./assets/trump_1.png")
+        },
+        {
+            name: "Xe cần cẩu đa năng",
+            shopName: "Thế giới đồ chơi",
+            isHotShop: false,
+            img: require("./assets/xa_can_cau.png")
+        },
+        {
+            name: "Đồ chơi dạng mô hình",
+            shopName: "Thế giới đồ chơi",
+            isHotShop: false,
+            img: require("./assets/do_choi_dang_mo_hinh.png")
+        },
+        {
+            name: "Lãnh đạo giản đơn",
+            shopName: "Minh Long Book",
+            isHotShop: false,
+            img: require("./assets/lanh_dao_gian_don.png")
+        },
+        {
+            name: "Hiểu lòng trẻ con",
+            shopName: "Minh Long Book",
+            isHotShop: false,
+            img: require("./assets/hieu_long_con_tre.png")
+        },
+        {
+            name: "Donal Trump thiên tài lãnh đạo",
+            shopName: "Minh Long Book",
+            isHotShop: false,
+            img: require("./assets/trump_1.png")
+        },
+    ])
+    const renderItemChat = (item, index)=>{
+        return (
+            <Pressable style={{flexDirection: "row", alignItems: "center", paddingVertical: 5
+            , backgroundColor: selectedID == index ? "#fff" : "transparent"}}
+                onPress={()=> setSelectedId(index)}
+            >
+                <View style={{flexDirection: "row", flex: 2}}>
+                    <Image
+                        source={item?.img}
+                        resizeMode='contain'
+                        style={{width: 74, height: 74, marginRight: 15, flex: 1}}
+                    />
+                    <View style={{flex: 2}}>
+                        <Text style={{fontSize: 16, marginBottom: 14, width: "100%"}} numberOfLines={1}
+                            
+                        >{item.name}</Text>
+                        <View style={{flexDirection: "row" ,alignItems: "center"}}>
+                            <Text style={{marginRight: 5}}>Shop</Text>
+                            <Text style={{color: item.isHotShop ? "#FF0E0E" : "#000"}}>{item.shopName}</Text>
+                        </View>
+                    </View>
+                </View>
+                <Pressable style={{flex: 1}}>
+                    <Text style={{backgroundColor: "#F31111", color: "#fff"
+                    , width: "70%", textAlign: "center", paddingVertical: 10, paddingHorizontal: 20
+                    ,fontSize :18
+                    }}>Chat</Text>
+                </Pressable>
+            </Pressable>
+        )
 
-          <View style={styles.style2}>
-            <View style={styles.ngaiChat}>
-            <Text>Ban co thac mac voi san pham vua xem.Dung ngai<br/>
-                   chat voi shop
-            </Text>
+    }
+    const seperatorComponent = ()=>{
+        return (
+            <View style={{height: 0.5, width: "100%", backgroundColor: "#999"}}>
+
             </View>
-         </View>
+        )
+    }
+  return (
+    <View style={{flex: 1}}>
+        <StatusBar/>
+        <SafeAreaView style={{flex: 1, backgroundColor: "#e5e5e5"}}>
+            <ScrollView
+                style={{flex: 1}}
+                stickyHeaderIndices={[0]}
+            >
+                <View>
+                    <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"
+                    , backgroundColor: "#1BA9FF", paddingHorizontal: 15, paddingVertical: 15}}>
+                        <Pressable
+                            onPress={()=>navigation.goBack()}
+                        >
+                            <Image
+                                source={require("./assets/btnBack.png")}
+                                resizeMode='contain'
+                                style={{width: 24, height: 24}}
+                            />
+                           
+                        </Pressable>
+                        <Text style={{color: "#fff", fontSize: 18}}>Chat</Text>
+                        <Pressable>
+                            <Image
+                                source={require("./assets/btnCart.png")}
+                                style={{width: 28, height: 28}}
+                                resizeMode='contain'
+                            />
+                        </Pressable>
+                    </View>
+                </View>
+                <View style={{paddingHorizontal: 40, paddingVertical: 10}}>
+                    <Text style={{fontSize: 16}}>Bạn có thắc mắc với sản phẩm vừa xem. Đừng ngại chat với shop!</Text>
+                </View>
 
-  <View style={styles.style3}>
-  
-  <View>
-    <Image source={require('./assets/ca_nau_lau.png')}
-    style={{width:74,height:74}}></Image>
-  </View>
-  <View style={styles.mini}>
-  <Text>Ca nau lau, nau mi mini</Text>
-  </View>
-  <View style={styles.mini1}>
-    <Pressable>
-  <Text style={{color:'red'}}>Shop Devang</Text>
-  </Pressable>
-  </View>  
-        <View style={styles.chat1}>
-          <TouchableOpacity>
-            <Text style={{color:'white'}}>Chat</Text>
-          </TouchableOpacity>
-        </View>
-
-</View>
-
-          <View style={styles.style4}>
-          <View>
-    <Image source={require('./assets/ga_bo_toi.png')}
-    style={{width:74,height:74}}></Image>
-  </View>
-  <View style={styles.mini}>
-  <Text>Ca nau lau, nau mi mini</Text>
-  </View>
-  <View style={styles.mini1}>
-    <Pressable>
-  <Text style={{color:'red'}}>Shop Devang</Text>
-  </Pressable>
-  </View>  
-        <View style={styles.chat1}>
-          <TouchableOpacity>
-            <Text style={{color:'white'}}>Chat</Text>
-          </TouchableOpacity>
-        </View>
-
-
-
-
-
-
-  </View>
-
-
-  <View style={styles.style5}>
-  <View>
-    <Image source={require('./assets/xa_can_cau.png')}
-    style={{width:74,height:74}}></Image>
-  </View>
-  <View style={styles.mini}>
-  <Text>Ca nau lau, nau mi mini</Text>
-  </View>
-  <View style={styles.mini1}>
-    <Pressable>
-  <Text style={{color:'red'}}>Shop Devang</Text>
-  </Pressable>
-  </View>  
-        <View style={styles.chat1}>
-          <TouchableOpacity>
-            <Text style={{color:'white'}}>Chat</Text>
-          </TouchableOpacity>
-        </View>
-
-  </View>
-
-  <View style={styles.style6}>
-  <View>
-    <Image source={require('./assets/do_choi_dang_mo_hinh.png')}
-    style={{width:74,height:74}}></Image>
-  </View>
-  <View style={styles.mini}>
-  <Text>Ca nau lau, nau mi mini</Text>
-  </View>
-  <View style={styles.mini1}>
-    <Pressable>
-  <Text style={{color:'red'}}>Shop Devang</Text>
-  </Pressable>
-  </View>  
-        <View style={styles.chat1}>
-          <TouchableOpacity>
-            <Text style={{color:'white'}}>Chat</Text>
-          </TouchableOpacity>
-        </View>
-  </View>
-
-  <View style={styles.style7}>
-  <View>
-    <Image source={require('./assets/lanh_dao_gian_don.png')}
-    style={{width:74,height:74}}></Image>
-  </View>
-  <View style={styles.mini}>
-  <Text>Ca nau lau, nau mi mini</Text>
-  </View>
-  <View style={styles.mini1}>
-    <Pressable>
-  <Text style={{color:'red'}}>Shop Devang</Text>
-  </Pressable>
-  </View>  
-        <View style={styles.chat1}>
-          <TouchableOpacity>
-            <Text style={{color:'white'}}>Chat</Text>
-          </TouchableOpacity>
-        </View>
-
+                <FlatList
+                    data={datas}
+                    keyExtractor={(item, index)=> index}
+                    renderItem={({item, index})=> renderItemChat(item, index)}
+                    extraData={selectedID}
+                    ItemSeparatorComponent={seperatorComponent}
+                />
+            </ScrollView>
+        </SafeAreaView>
     </View>
-
-
-      </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    backgroundImage:'linear-gradient(to bottom,white, cyan)'
-    },
-    ngaiChat:{
-      justifyContent:'center',
-      alignItems:'center',
-      alignContent:'center'
-     
-    
-    },
-mini:{
-   position:'absolute',
-   top:20,
-   left:100
-},
-mini1:{
-  position:'absolute',
-  top:50,
-   left:100
-},
-chat1:{
-  backgroundColor:'red',
-  width:88,
-  height:38,
-alignItems:'center',
-justifyContent:'center',
-  position:'absolute',
-  top:30,
-   left:280,
-   
-},
-
-    txt2:{
-    position:'absolute',
-    top:160,
-    left:25,
-    width:1000
-    },
-    gioHang:{
-      positionL:'absolute',
-      Button:10
-    },
-   
-    
-    btn5:{
-     backgroundColor:'#E53935',
-    height:50,
-    width:340,
-    position:'absolute',
-    left:35,
-    top :120
-    },
-  
-    btn3:{
-       backgroundColor:'#0A5EB7',
-       width:75,
-       height:40,
-       padding:8,
-       position:'absolute',
-       top:200,
-       left:250
-    },
-
-    tien:{
-        position:'absolute',
-        top:30,
-        left:300
-    },
-    tamtinh:{
-     position:'absolute',
-     top:30,
-     left:20
-    },
-    thanhTien:{
-        position:'absolute',
-        top:75,
-        left:20,
-        
-      },
-      tien1:{
-        position:'absolute',
-        top:1,
-        left:300
-      },
-    magiamgia:{
-      borderColor:'black',
-      borderWidth:'1px',
-      width:200,
-      height:45,
-      position:'absolute',
-      top:200,
-      padding:10,
-      flexDirection:'row'
-    },
-    magiamgia1:{
-backgroundColor:'#F2DD1B',
-height:25,
-width: 50,
-
-
-    },
-    style1:{
-  flex:0.2,
-  backgroundColor:'#1BA9FF'
-    },
-   pic:{
-    position:'absolute',
-    width:150,
-    height:150,
-    left:10,
-    top: 10
-    },
-    link:{
-        position:'absolute',
-       
-        left:150,
-   color:'blue'
-    },
-Text:{
-    position:'absolute',
-    left:165
-},
-btn1:{
-    backgroundColor:'gray',
-    width:'30px'
-},
-btn2:{
-    backgroundColor:'gray',
-    width:'30px',
-    position:'absolute',
-    left:65,
-    top:100
-
-},
-textstyle3:{
-    
-    position:'absolute',
-    top:30,
-    left:20,
-    flexDirection:'row'
-},
-    style2:{
-        flex:0.3,
-        backgroundColor:'#C4C4C4',
-        justifyContent:'center'
-    },
-    style3:{
-        flex:0.5,
-        backgroundColor:'white',
-        borderWidth:1,
-        borderColor:'gray'
-
-    },
- 
-    
-    style4:{
-        flex:0.5,
-        backgroundColor:'white',
-        borderWidth:1,
-        borderColor:'lightgray'
-    },
-    style5:{
-        flex:0.5,
-        backgroundColor:'white'
-      },
-      style6:{
-        flex:1,
-        backgroundColor:'white'
-    },
-    style7:{
-        flex:1,
-        backgroundColor:'white'
-      },
-
-
-
-
-
-
-
-
-
-
-
-  });
-  
+  )
+}
